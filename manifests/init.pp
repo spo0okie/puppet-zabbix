954db032d1c4982f1da86c7e3c4d9b9d8c41f0dc
root <root@msk-inventory.yamalgazprom.local>
2019-05-23 21:37:16 +0500

    centos repo updated to 3.4

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,18 @@
 class zabbix {
 	case $::operatingsystem {
-		FreeBSD: {
-			$packagename='zabbix32-agent'
-			$servicename='zabbix_agentd'
-		}
 		CentOS: {
 			yumrepo { 'zabbix32_repo':
+				name	=>	"Zabbix Official Repository - ${::architecture}"
 				enabled	=>	1,
-				baseurl	=>	"http://repo.zabbix.com/zabbix/3.2/rhel/${::operatingsystemmajrelease}/${::architecture}",
+				baseurl	=>	"http://repo.zabbix.com/zabbix/3.4/rhel/${::operatingsystemmajrelease}/${::architecture}",
 				gpgcheck=>	0,
 				before	=>	Package['zabbix-agent']
+			}
+			yumrepo { 'zabbix_non_supported':
+				name	=>	"Zabbix Official Repository non-supported - ${::architecture}"
+				baseurl	=>	"http://repo.zabbix.com/non-supported/rhel/7/${::architecture}/"
+				enabled=1
+				gpgcheck=1
 			}
 			$packagename='zabbix-agent'
 			$servicename='zabbix-agent'
