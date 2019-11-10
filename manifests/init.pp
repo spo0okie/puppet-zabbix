e8718feb9a10cb71050b6b41c4d07b62f4a92f35
root <root@msk-inventory.yamalgazprom.local>
2019-11-10 14:54:13 +0500

    Поправлена репа

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,17 +2,18 @@ class zabbix {
 	case $::operatingsystem {
 		CentOS: {
 			yumrepo { 'zabbix32_repo':
-				name	=>	"Zabbix Official Repository - ${::architecture}"
+				descr	=>	"Zabbix Official Repository - ${::architecture}",
 				enabled	=>	1,
 				baseurl	=>	"http://repo.zabbix.com/zabbix/3.4/rhel/${::operatingsystemmajrelease}/${::architecture}",
 				gpgcheck=>	0,
 				before	=>	Package['zabbix-agent']
 			}
 			yumrepo { 'zabbix_non_supported':
-				name	=>	"Zabbix Official Repository non-supported - ${::architecture}"
-				baseurl	=>	"http://repo.zabbix.com/non-supported/rhel/7/${::architecture}/"
-				enabled=1
-				gpgcheck=1
+				descr	=>	"Zabbix Official Repository non-supported - ${::architecture}",
+				baseurl	=>	"http://repo.zabbix.com/non-supported/rhel/7/${::architecture}/",
+				enabled	=>	1,
+				gpgcheck=>	1,
+				before	=>	Package['zabbix-agent']
 			}
 			$packagename='zabbix-agent'
 			$servicename='zabbix-agent'
